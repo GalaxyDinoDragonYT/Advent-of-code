386da3464103796fc057d2bf9e66c1923f261bec
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-12-01 19:36:38 +0000

    Update day_1.py

--- day 1/day_1.py
+++ day 1/day_1.py
@@ -1,4 +1,3 @@
-i = 
 v = 0
 file1 = open("tested.txt","a")
 
@@ -8,3 +7,13 @@ def order(n)
      print(f'{n} Increased')
    elif n - v < 0:
     print(f'{n} Decreased'
+
+ 
+with open(file1) as f:
+  line = f.readline()
+  i = 2000
+  b = 1
+  while i => 1:
+    i = i - 1
+    order(file[b])
+    b = b + 1
